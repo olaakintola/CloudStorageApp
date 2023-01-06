b4e6d766cb87822ca961a4ae938c35612b9eeb76
Ola Akintola <olanipekun_akin@yahoo.co.uk>
2023-01-06 12:48:07 +0000

    configure the maximum file size that can be uploaded on the app

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/exception/FileUploadExceptionAdvice.java
@@ -0,0 +1,17 @@
+package com.udacity.jwdnd.course1.cloudstorage.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxSizeException(Model model, MaxUploadSizeExceededException maxUploadSizeExceededException){
+
+        model.addAttribute("uploadFileError", "File is too large!");
+        return "result";
+    }
+}
